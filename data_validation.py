d6ff4dbab4fc23846239e9c26da918c1fb711466
Denise <deniselewis1120@gmail.com>
2025-01-04 12:46:07 -0800

    Add data quality checks and error handling

--- data_validation.py
+++ data_validation.py
@@ -1,4 +1,6 @@
-def run_data_validation(transactions_df, categories_df):
+import pandas as pd
+
+def explore_data(transactions_df, categories_df):
     print("Transactions DataFrame:\n")
     print(transactions_df.head(), "\n")
     print(transactions_df.info(), "\n")
@@ -15,3 +17,55 @@ def run_data_validation(transactions_df, categories_df):
     print("Duplicated Rows:\n", categories_df[categories_df.duplicated()], "\n")
     print("Rows with NULL values:\n", categories_df[categories_df.isnull().any(axis=1)], "\n")
     print("Rows with NA values:\n", categories_df[categories_df.isna().any(axis=1)], "\n")
+
+def validate_categories(df):
+    # Check for expected columns
+    expected_columns = ["id", "name", "type"]
+    missing_columns = set(expected_columns) - set(df.columns)
+    if missing_columns:
+        raise ValueError(f"Missing column(s): {', '.join(missing_columns)}")
+
+    # Check for duplicates
+    df["type"] = df["type"].str.lower()
+    columns_to_check = ["id"]
+    df = df.drop_duplicates(subset=columns_to_check, keep="first")
+    columns_to_check = ["name", "type"]
+    df = df.drop_duplicates(subset=columns_to_check, keep="first")
+
+    # Check for NULLS and NAs
+    if df.isnull().values.any() or df.isna().values.any():
+        raise ValueError("Data contains NULL (NaN) value(s).")
+
+    # Check for invalid ids
+    if df["id"].dtype != "int64":
+        raise ValueError(f"Invalid id(s) found, ids must be an integer.")
+
+    # Check for invalid transaction types
+    valid_types = ["income", "expense"]
+    invalid_types = df[~df["type"].isin(valid_types)]
+    if not invalid_types.empty:
+        raise ValueError(f"Invalid type(s) found: {', '.join(invalid_types["type"].unique())}")
+
+def validate_transactions(df):
+    # Check for expected columns
+    expected_columns = ["date", "name", "category", "amount", "account"]
+    missing_columns = set(expected_columns) - set(df.columns)
+    if missing_columns:
+        raise ValueError(f"Missing column(s): {', '.join(missing_columns)}")
+    
+    # Check for duplicates
+    df = df.drop_duplicates(keep="first")
+
+    # Check for NULLS and NAs
+    if df.isnull().values.any() or df.isna().values.any():
+        raise ValueError("Data contains NULL (NaN) value(s).")
+
+    # Check for invalid dates
+    try:
+        df["date"] = pd.to_datetime(df["date"], format="%m/%d/%y")
+    except:
+            raise ValueError(f"Invalid date, date must be in mm/dd/yy format.")
+
+    # Check for invalid amounts
+    if df["amount"].dtype != "float64":
+       raise ValueError(f"Invalid amount(s) found, amounts must be a float.")
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import sqlite3
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-from data_validation import run_data_validation
+from data_validation import *
 from dashboard import *
 
 def main():
@@ -12,7 +9,11 @@ def main():
     categories_df = pd.read_csv("categories.csv")
 
     # Explore data files
-    #run_data_validation(transactions_df, categories_df)
+    #explore_data(transactions_df, categories_df)
+
+    # Validate data files
+    validate_categories(categories_df)
+    validate_transactions(transactions_df)
 
     # Connect to database
     conn = sqlite3.connect("finance.db")
@@ -40,32 +41,31 @@ def main():
     categories_df.to_sql("categories", conn, if_exists="replace", index=False)
 
     categories_df = pd.read_sql("SELECT * FROM categories", conn)
-    category_mapping = dict(zip(categories_df['name'], categories_df['id']))
+    category_mapping = {}
+    for index, row in categories_df.iterrows():
+        category_mapping[(row["name"], row["type"])] = row["id"]
 
-    other_expense_id = category_mapping.get("Other")
-    other_income_id = category_mapping.get("Other Income")
+    other_expense_id = category_mapping.get(("Other", "expense"))
+    other_income_id = category_mapping.get(("Other Income", "income"))
 
 
     def get_category_id(row):
-        category_name = row['category']
-        amount = row['amount']
+        category_name = row["category"]
+        amount = row["amount"]
+
+        type = "income" if amount > 0 else "expense"
         
-        if category_name in category_mapping:
-            return category_mapping[category_name]
+        if (category_name, type) in category_mapping:
+            return category_mapping[(category_name, type)]
         
         else:
-            return other_income_id if amount > 0 else other_expense_id
+            return other_income_id if type == "income" else other_expense_id
 
-
-    ################################
-    # DATA CHECKS HERE
-    ################################
 
     # Insert data into transactions table
-    transactions_df['category_id'] = transactions_df.apply(get_category_id, axis=1)
-    transactions_df.drop(columns=['category'], inplace=True)
-    transactions_df['date'] = pd.to_datetime(transactions_df['date'], format='%m/%d/%y')
-    transactions_df.to_sql('transactions', conn, if_exists='replace', index=False)
+    transactions_df["category_id"] = transactions_df.apply(get_category_id, axis=1)
+    transactions_df.drop(columns=["category"], inplace=True)
+    transactions_df.to_sql("transactions", conn, if_exists="replace", index=False)
 
 
     # Query transactions
@@ -87,53 +87,53 @@ def main():
     df = pd.read_sql(query, conn)
 
     # Format date and create month_year column
-    df['date'] = pd.to_datetime(df['date'])
-    df['month_year'] = df['date'].dt.to_period('M')
+    df["date"] = pd.to_datetime(df["date"])
+    df["month_year"] = df["date"].dt.to_period("M")
 
     print(df)
 
 
     # Grouping and aggregation by month
     monthly_aggregated_df = (
-        df.groupby(['month_year'])
+        df.groupby(["month_year"])
         .agg(
-            income=('amount', lambda x: x[df.loc[x.index, 'type'] == 'income'].sum()),
-            expenses=('amount', lambda x: x[df.loc[x.index, 'type'] == 'expense'].sum())
+            income=("amount", lambda x: x[df.loc[x.index, "type"] == "income"].sum()),
+            expenses=("amount", lambda x: x[df.loc[x.index, "type"] == "expense"].sum())
         )
         .reset_index()
     )
 
     # Add abs_expenses, net_balance, month_year_str, month columns
-    monthly_aggregated_df['abs_expenses'] = monthly_aggregated_df['expenses'].abs()
-    monthly_aggregated_df['net_balance'] = monthly_aggregated_df['income'] + monthly_aggregated_df['expenses']
-    monthly_aggregated_df['month_year_str'] = monthly_aggregated_df['month_year'].astype(str)
-    monthly_aggregated_df['month'] = monthly_aggregated_df['month_year'].dt.strftime('%b')
+    monthly_aggregated_df["abs_expenses"] = monthly_aggregated_df["expenses"].abs()
+    monthly_aggregated_df["net_balance"] = monthly_aggregated_df["income"] + monthly_aggregated_df["expenses"]
+    monthly_aggregated_df["month_year_str"] = monthly_aggregated_df["month_year"].astype(str)
+    monthly_aggregated_df["month"] = monthly_aggregated_df["month_year"].dt.strftime("%b")
 
     print(monthly_aggregated_df)
 
 
     # Filter data for last two years
-    current_year = monthly_aggregated_df['month_year'].dt.year.max()
+    current_year = monthly_aggregated_df["month_year"].dt.year.max()
     last_year = current_year - 1
-    current_year_data = monthly_aggregated_df[monthly_aggregated_df['month_year'].dt.year == current_year]
-    last_year_data = monthly_aggregated_df[monthly_aggregated_df['month_year'].dt.year == last_year]
+    current_year_data = monthly_aggregated_df[monthly_aggregated_df["month_year"].dt.year == current_year]
+    last_year_data = monthly_aggregated_df[monthly_aggregated_df["month_year"].dt.year == last_year]
 
     # Sort data by month
-    current_year_data = current_year_data.sort_values('month_year')
-    last_year_data = last_year_data.sort_values('month_year')
+    current_year_data = current_year_data.sort_values("month_year")
+    last_year_data = last_year_data.sort_values("month_year")
     
     # Calculate totals
-    total_income = round(df[df['type'] == 'income']['amount'].sum(), 2)
-    total_expenses = round(df[df['type'] == 'expense']['amount'].sum(), 2)
+    total_income = round(df[df["type"] == "income"]["amount"].sum(), 2)
+    total_expenses = round(df[df["type"] == "expense"]["amount"].sum(), 2)
     net_balance = round(total_income + total_expenses, 2)
 
     # Group by category
-    expenses_by_category = (abs(df[df['type'] == 'expense'].groupby('category_name')['amount'].sum()).sort_values(ascending=False))
-    income_by_category = (abs(df[df['type'] == 'income'].groupby('category_name')['amount'].sum()).sort_values(ascending=False))
+    expenses_by_category = (abs(df[df["type"] == "expense"].groupby("category_name")["amount"].sum()).sort_values(ascending=False))
+    income_by_category = (abs(df[df["type"] == "income"].groupby("category_name")["amount"].sum()).sort_values(ascending=False))
 
     # Group by month
-    expenses_by_month = (abs(df[df['type'] == 'expense'].groupby('month_year')['amount'].sum()))
-    income_by_month = (abs(df[df['type'] == 'income'].groupby('month_year')['amount'].sum()))
+    expenses_by_month = (abs(df[df["type"] == "expense"].groupby("month_year")["amount"].sum()))
+    income_by_month = (abs(df[df["type"] == "income"].groupby("month_year")["amount"].sum()))
     net_balance_by_month = round(income_by_month - expenses_by_month, 2)
 
     print(f"Total Income: ${total_income}")
